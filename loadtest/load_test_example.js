c37e5e5b3fc63f381ad087a4a1829890fb48e577
Prakhar Agnihotri <rounak316@gmail.com>
2020-08-03 12:19:22 +0530

    sg -> meddler: repo's remote  switch

--- loadtest/load_test_example.js
+++
@@ -1,8 +0,0 @@
-import { check, sleep } from "k6";
-import http from "k6/http";
-export default function () {
-    let res = http.get("http://quick.meddler.xyz:443/");
-    check(res, {
-        "is status 200": (r) => r.status === 200
-    });
-};
